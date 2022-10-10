932a15edcb38505ba252eb5c1e9e049a819e68c2
shreyash304 <2013shreyash@gmail.com>
2022-10-10 23:50:26 +0530

    initial commit

---
+++ story_generator.py
@@ -0,0 +1,21 @@
+import random
+  
+def generate_story():  
+    Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time']
+    character = [' there lived a king.',' there was a man named Jack.',
+                ' there lived a farmer.']
+    time = [' One day', ' One full-moon night','One morning']
+    story_plot = [' he was passing by',' he was going for a picnic to ']
+    place = [' the mountains', ' the garden']
+    second_character = [' he saw a man', ' he saw a young lady']
+    age = [' who seemed to be in late 20s', ' who seemed very old and feeble']
+    work = [' searching something.', ' digging a well on roadside.']
+    
+    # Selecting an item from each list and concatenating them.
+    print(random.choice(Sentence_starter)+random.choice(character)+
+        random.choice(time)+random.choice(story_plot) +
+        random.choice(place)+random.choice(second_character)+
+        random.choice(age)+random.choice(work))
+
+if __name__=="__main__":
+    generate_story()
